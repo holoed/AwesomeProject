1996beeae149dc688b6ef4ab82382e6b3098044f
holoed <edmondo@gmail.com>
2015-05-13 01:00:34 +0100

    Added video support

--- src/MovieDetails/index.js
+++ src/MovieDetails/index.js
@@ -5,8 +5,22 @@ var {
   View,
   Text,
   Image,
-  StyleSheet
+  StyleSheet,
+  TouchableHighlight
 } = React;
+
+var Video = require('react-native-video');
+
+var MovieVideo = React.createClass({
+    render: function() {
+        return ( <Video source={{uri: this.props.post.sources[0]}}
+                        style={styles.video}
+                        resizeMode="contain"></Video>
+          
+           
+        );
+    }  
+});
 
 var ForTouchScene = React.createClass({
     getInitialState: function() {
@@ -29,15 +43,23 @@ var ForTouchScene = React.createClass({
         })
         .done();
     },
+    onPress: function() {
+        this.props.navigator.push({
+            title: this.props.post.title,
+            component: MovieVideo,
+            passProps: { post : this.props.post },
+        });
+    },
     render: function() {
-        return (
-            <View>
-                <Image
-              source={{uri:this.props.post["image-480x270"]}}
-              style={styles.cellImage}/>
-                <Text style={{fontWeight: 'bold'}}>{this.props.post.title}</Text>
-                <Text>{this.state.movieDetails.Plot}</Text>
-            </View>
+        return ( <View>
+                  <TouchableHighlight 
+                         onPress={this.onPress}>
+                    <Image source={{uri:this.props.post["image-480x270"]}}
+                           style={styles.cellImage} />
+                  </TouchableHighlight>
+                  <Text style={{fontWeight: 'bold', fontSize:'30'}}>{this.state.movieDetails.Title}</Text>
+                  <Text>{this.state.movieDetails.Plot}</Text>
+                </View>          
         );
     }
 });
@@ -49,6 +71,14 @@ var styles = StyleSheet.create({
     height: 300,
     marginRight: 10,
     width: 210,
+  },
+   video: {
+    backgroundColor: '#dddddd',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   }
 });
 
